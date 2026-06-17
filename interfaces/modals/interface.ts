a76c2b21d754d47fe6ac39069e7c0c4c8e74b27f
isaackd <isaackd331@gmail.com>
2026-06-17 09:40:14 +0900

    Zustand 도입 후 필요없는 Hook 파일 삭제, interface 선언 및 modal 전역 관리 Store 선언

--- interfaces/modals/interface.ts
+++ interfaces/modals/interface.ts
@@ -11,3 +11,29 @@ export interface IModal {
   children: React.ReactNode;
   title?: string;
 }
+
+/**
+ * openModal 인터페이스
+ * - @required content[ReactNode]: 모달 내용 구성을 위한 컨텐츠
+ * - title: 모달 제목
+ */
+interface IOpenModal {
+  content: React.ReactNode;
+  title?: string;
+}
+
+/**
+ * ModalStore 인터페이스
+ * - @required isOpen[boolean]: 모달 Open 여부 체크
+ * - @required content[ReactNode]: 모달 내용 구성을 위한 컨텐츠
+ * - @required openModal[(params: IOpenModal) => void]: 모달 열기 함수
+ * - @required closeModal[() => void]: 모달 닫기 함수
+ * - title[string]: 제목
+ */
+export interface IModalStore {
+  isOpen: boolean;
+  content: React.ReactNode;
+  openModal: (params: IOpenModal) => void;
+  closeModal: () => void;
+  title?: string;
+}
