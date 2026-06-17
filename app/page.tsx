edc7fb5cb92c1155aa7b1ab636cf10a8b3c71d11
isaackd <isaackd331@gmail.com>
2026-06-17 09:41:15 +0900

    모달 호출 테스트 케이스 Index Page에 추가

--- app/page.tsx
+++ app/page.tsx
@@ -3,43 +3,50 @@
 import Button from "@/components/common/Button";
 import ToggleDarkBtn from "@/components/headers/ToggleDarkBtn";
 
+import { useModalStore } from "@/zustand/useModalStore";
+
 const Index = () => {
+  const { openModal } = useModalStore();
+
+  const handleOpenModal = () => {
+    openModal({
+      content: <div>테스트</div>,
+    });
+  };
+
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex gap-4">
-        <Button
-          label="프라이머리 버튼 테스트"
-          size="fit"
-          variant="primary"
-          onClick={() => {
-            console.log("프라이머리 버튼 반응중");
-          }}
-        />
-
-        <Button
-          label="오렌지 버튼 테스트"
-          size="fit"
-          variant="orange"
-          onClick={() => {
-            console.log("오렌지 버튼 반응중");
-          }}
-        />
-
-        <Button
-          disabled
-          label="비활성 버튼 테스트"
-          size="fit"
-          variant="orange"
-          onClick={() => {
-            console.log("반응이 되면 곤란");
-          }}
-        />
-
-        <ToggleDarkBtn />
+    <>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <Button
+            label="프라이머리 버튼 테스트"
+            size="fit"
+            variant="primary"
+            onClick={() => {
+              handleOpenModal();
+            }}
+          />
+          <Button
+            label="오렌지 버튼 테스트"
+            size="fit"
+            variant="orange"
+            onClick={() => {
+              console.log("오렌지 버튼 반응중");
+            }}
+          />
+          <Button
+            disabled
+            label="비활성 버튼 테스트"
+            size="fit"
+            variant="orange"
+            onClick={() => {
+              console.log("반응이 되면 곤란");
+            }}
+          />
+          <ToggleDarkBtn />
+        </div>
       </div>
-
-      <div>테스트</div>
-    </div>
+    </>
   );
 };
 
